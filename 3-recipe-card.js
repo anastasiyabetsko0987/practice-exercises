a64e3657d9d280383c940290dfb4247757db7240
Anastasiia Kuzko <anastasiyabetsko0987@gmail.com>
2025-05-11 11:17:28 +0200

    added file 3-recipe-card.js

---
+++ 3-recipe-card.js
@@ -0,0 +1,30 @@
+/**
+ * Ever wondered how to make a certain meal? Let's create a recipe list with JavaScript!
+ *   
+ *   Declare a variable that holds an empty object literal (your meal recipe).
+ *   Give the object 3 properties: a title (string), a servings (number) and an ingredients (array of strings) property.
+ *   Log each property out separately, using a loop (for, while or do/while)
+ *   
+ * Expected result:
+ *   
+ *   Meal name: Omelette
+ *   Serves: 2
+ *   Ingredients: 4 eggs, 2 strips of bacon, 1 tsp salt/pepper
+ */
+
+const recipes = {
+  title: 'Omelette',
+  servings: 2,
+  ingredients: ['4 eggs', '2 strips of bacon', '1 tsp salt/pepper']
+};
+
+for (const key in recipes) {
+  if (key == 'title') {
+    console.log(`Meal name: ${recipes[key]}`);
+  } else if (key == 'servings') {
+    console.log(`Serves: ${recipes[key]}`);
+  } else if (key == 'ingredients') {
+    console.log(`Ingredients: ${recipes[key].join(', ')}`);
+  }
+}
+
