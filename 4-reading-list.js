5e2de3eb063a72c8394c9a18baba6e670944f236
Anastasiia Kuzko <anastasiyabetsko0987@gmail.com>
2025-05-11 21:51:08 +0200

    added file 4-reading-list.js

---
+++ 4-reading-list.js
@@ -0,0 +1,37 @@
+/**
+ * Keep track of which books you read and which books you want to read!
+ *  
+ * Follow the steps:
+ *  Declare a variable that holds an array of 3 objects, where each object describes a book and has properties for the title (string), author (string), and alreadyRead (boolean indicating if you read it yet).
+ *  Loop through the array of books.
+ *  For each book, log the book title and book author like so: "The Hobbit by J.R.R. Tolkien".
+ *  Create a conditional statement to change the log depending on whether you read it yet or not. If you read it, log a string like You already read "The Hobbit" right after the log of the book details
+ *  If you haven't read it log a string like You still need to read "The Lord of the Rings"
+ */
+
+const books = [
+  {
+    title: 'The Hobbit',
+    author: 'J.R.R. Tolkien',
+    alreadyRead: false
+  },
+  {
+    title: "Harry Potter and the Philosopher's Stone",
+    author: 'J. K. Rowling',
+    alreadyRead: false
+  },
+  {
+    title: 'Pride and Prejudice',
+    author: 'Jane Austen',
+    alreadyRead: true
+  }
+];
+books.forEach((el)=> {
+  console.log(`${el.title} by ${el.author}`);
+  if (!el.alreadyRead) {
+    console.log('You still need to read');
+  } else {
+    console.log('You already read');
+  }
+});
+  
